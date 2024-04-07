7942f57c95eff64da6c34e1660afc59b97ab3fff
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-04-07 13:54:25 -0300

    App.js cambio al anterior.

--- front/App.js
+++ front/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home} from "./pages/Home";
 import {Login} from "./pages/Login"
 import {Register} from "./pages/Register"
+import {Home} from "./pages/Home";
+import {UnlockedScreen} from "./pages/UnlockedScreen";
 export default function App() {
-  const Stack = createNativeStackNavigator();
+    const Stack = createNativeStackNavigator(); //used to configure the screens
 
-  return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home}/>
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="Register" component={Register}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="Register" component={Register}/>
+                <Stack.Screen name="UnlockedScreen" component={UnlockedScreen}/>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
