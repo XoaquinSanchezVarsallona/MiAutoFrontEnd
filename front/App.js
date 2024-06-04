212386f720fa8fac7edd746b958edfc578226ea2
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-06-04 12:23:03 -0300

    Hice el display de las fotos y tambien que descargue la foto.

--- front/App.js
+++ front/App.js
@@ -45,7 +45,7 @@ import AppNotification from "./components/notification/AppNotification";
 import {ServiceConfigurationScreen} from "./pages/serviceScreens/serviceConfiguration/ServiceConfigurationScreen";
 import {ServiceEditProfile} from "./pages/serviceScreens/serviceConfiguration/ServiceEditProfile";
 import {ServiceViewProfile} from "./pages/serviceScreens/serviceConfiguration/ServiceViewProfile";
-import {DisplayPapers} from "./pages/driverScreens/vehicleScreens/DisplayPapers";
+
 
 
 const Stack = createNativeStackNavigator(); //used to configure the screens
@@ -129,7 +129,7 @@ function AppNavigation() {
                                     <Stack.Screen name="ServiceConfigurationScreen" component={ServiceConfigurationScreen}/>
                                     <Stack.Screen name="ServiceViewProfile" component={ServiceViewProfile}/>
                                     <Stack.Screen name="ServiceEditProfile" component={ServiceEditProfile}/>
-                                    <Stack.Screen name="DisplayPapers" component={DisplayPapers}/>
+
                                 </>
                             ) : (
                                 <>
@@ -168,7 +168,6 @@ function AppNavigation() {
                                     <Stack.Screen name="ServiceConfigurationScreen" component={ServiceConfigurationScreen}/>
                                     <Stack.Screen name="ServiceViewProfile" component={ServiceViewProfile}/>
                                     <Stack.Screen name="ServiceEditProfile" component={ServiceEditProfile}/>
-                                    <Stack.Screen name="DisplayPapers" component={DisplayPapers}/>
                                 </>
                             )}
                         </Stack.Navigator>
